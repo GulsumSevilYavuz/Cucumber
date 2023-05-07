cfde68aab8e91344cadd7043f03f1b0bfa350271
Gülsüm Sevil Yavuz <gulsumyavuz54@gmail.com>
2023-05-07 18:29:50 +0200

    v1

---
+++ src/test/java/techproed/runner/Runner.java
@@ -0,0 +1,15 @@
+package techproed.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features ="src/test/resources/features",glue = {"techproed/stepDefinitions"},tags = "g1"
+
+)
+
+
+public class Runner {
+
+}
